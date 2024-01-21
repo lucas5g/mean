162369a3707b49f1c0e832bb8258f073774d70ae
lucas.sousa <lucas.sousa@codificar.com.br>
2024-01-21 16:37:10 -0300

    api express

--- server/server.ts
+++ server/server.ts
@@ -1,9 +1,41 @@
-import express from 'express'
+import express, { NextFunction, Request, Response } from 'express'
+import { PrismaClient } from '@prisma/client'
+import { z } from 'zod'
+const prisma = new PrismaClient()
 
 const app = express()
 
-app.get('/', (req, res) => {
-  res.json({message:' api server'})
+app.get('/', async (req, res) => {
+  res.json(await prisma.word.findMany())
+})
+
+app.post('/', async (req, res) => {
+  const data = z.object({
+    name: z.string(),
+    meaning: z.string(),
+    fixed: z.boolean()
+  }).parse(req.body)
+
+  res
+    .status(201)
+    .json(await prisma.word.create({
+      data
+    }))
+})
+
+app.delete('/:id', async(req, res) => {
+  res 
+    .status(204)
+    .json(await prisma.word.delete({
+      where:{
+        id: req.params.id
+      }
+    }))
+})
+
+
+app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
+  res.json({ msg: 'qweqwew' })
 })
 
 app.listen(8000, () => console.log('HTTP SERVER RUNNING!'))
