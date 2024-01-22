6481ddd5516d123a2b825634cc68460c7d3dde95
lucas.sousa <lucas.sousa@codificar.com.br>
2024-01-22 20:27:55 -0300

    install tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -13,15 +13,15 @@ interface WordInterface {
 export function App() {
   const [fixeds, setFixeds] = useState([] as string[]);
 
-  const { data, error, isLoading} = swr('api')
+  const { data, error, isLoading } = swr('api')
 
-  if(error) return <div>Erro ao carregar.</div>
-  if(isLoading) return <div>Carregando...</div>
-  
-  const words:WordInterface[] = data
+  if (error) return <div>Erro ao carregar.</div>
+  if (isLoading) return <div>Carregando...</div>
+
+  const words: WordInterface[] = data
 
   return (
-    <main className='h-screen p-10 space-y-8 text-white bg-gray-800'>
+    <main className='min-h-screen p-10 space-y-8 text-white bg-gray-800'>
       <input
         placeholder="Type words"
         className="w-full p-5 font-semibold text-white bg-gray-600 border rounded-xl placeholder:text-white"
@@ -44,26 +44,34 @@ export function App() {
       />
 
       <ul
-        className={clsx('grid grid-cols-3 lg:grid-cols-5 gap-2', {
+        className={clsx('grid grid-cols-3 lg:grid-cols-5 gap-5', {
           'grid-cols-1': words.length === 1,
           'grid-cols-2 lg:grid-cols-2': words.length === 2,
         })}
       >
         {words.map((word) => {
           return (
-            <li key={word.id}>
-              <button
+            <button
+              key={word.id}
+              className={clsx('p-3  border rounded-xl ', {
+                'bg-gray-600': word.fixed,
+              })}  >
+              <li
+                // className='h-30'
+              >
+                {/* <button
                 className={clsx('p-3  border rounded-xl overflow-hidden', {
                   'bg-gray-600': word.fixed,
                 })}
                 onClick={() => {
                   setFixeds([...fixeds, word.id]);
                 }}
-              >
+              > */}
                 <strong className="pr-2">{word.name.toUpperCase()}</strong>
                 <div>{word.meaning}</div>
-              </button>
-            </li>
+                {/* </button> */}
+              </li>
+            </button>
           );
         })}
       </ul>
